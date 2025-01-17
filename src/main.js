59862c41d0054470eb98e9b2e4b3524dd7f5693a
Arunjadhav0101 <arunj5687@gmail.com>
2025-01-17 16:11:45 +0530

    added js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
 const msg = document.querySelector('.massege-input');
+const btn = document.querySelector('.button');
+const remove = document.querySelector('.button-restart');
 
-msg.addEventListener('input', (event) => {
-  console.log('User is typing:', event.target.value);
+
+
+remove.addEventListener('click', () => {
+  const output = document.querySelector('.message-output');
+  if (output.children.length>0) {
+    output.removeChild(output.lastElementChild);
+
+  }
+  // output.innerHTML = '';
 });
+
+
+btn.addEventListener('click', () => {
+  const output = document.querySelector('.message-output');
+  if (msg.value.trim() !== "") {
+    const li = document.createElement('li')
+    li.textContent =msg.value
+    output.appendChild(li);
+    msg.value = "";
+  }
+  // output.innerHTML = msg.value;
+  //  msg.value = "";
+ });
